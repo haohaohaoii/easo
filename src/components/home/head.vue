8cf58e6efd04ca5475301d411fe707e58b060a5f
haohaohaoii <641882555@qq.com>
2019-12-19 13:58:50 +0800

    百分比布局

---
+++ src/components/home/head.vue
@@ -0,0 +1,78 @@
+<template>
+    <!--头部菜单-->
+
+    <div class="header">
+        <!--头部右侧菜单-->
+        <div class="headerR">
+            <img src="../../assets/images/admin.png" alt />
+
+            <img src="../../assets/images/msg.png" alt />
+
+            <img src="../../assets/images/change.png" alt />
+
+            <img src="../../assets/images/out.png" alt />
+        </div>
+        <!--头部左侧菜单-->
+        <el-menu
+            :default-active="activeIndex"
+            class="el-menu-demo headerL"
+            mode="horizontal"
+            @select="handleSelect"
+            text-color="#333333"
+            active-text-color="#5B8CFF"
+        >
+            <el-menu-item index="1">数据查看</el-menu-item>
+            <el-submenu index="2">
+                <template slot="title">企业管理</template>
+                <el-menu-item index="2-1">选项1</el-menu-item>
+                <el-menu-item index="2-2">选项2</el-menu-item>
+                <el-menu-item index="2-3">选项3</el-menu-item>
+                <el-submenu index="2-4">
+                    <template slot="title">选项4</template>
+                    <el-menu-item index="2-4-1">选项1</el-menu-item>
+                    <el-menu-item index="2-4-2">选项2</el-menu-item>
+                    <el-menu-item index="2-4-3">选项3</el-menu-item>
+                </el-submenu>
+            </el-submenu>
+            <el-menu-item index="3">权限管理</el-menu-item>
+        </el-menu>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            activeIndex: "1"
+        };
+    },
+    methods: {
+        handleSelect() {}
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+.header {
+    display: flex;
+    align-items: center;
+    .headerL {
+        width: 75%;
+    }
+    .headerR {
+        width: 25%;
+        display: flex;
+        justify-content: space-around;
+        img {
+            width: 20px;
+            height: 20px;
+        }
+    }
+}
+// .header {
+//     .headerR {
+//         position: relative;
+//         right: 0;
+//     }
+// }
+</style>
---
+++ src/components/home/home.vue
@@ -0,0 +1,47 @@
+<template>
+    <!--首页面-->
+    <div class="home">
+        <!--左部导航栏组件-->
+        <left-nav class="contentL"></left-nav>
+        <!--右侧内容-->
+        <div class="contentR">
+            <!--头部导航组件-->
+            <head-nav></head-nav>
+            <!--内容组件-->
+            <div class="home-content"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+import head from "./head"; //引入头部组件
+import left from "./left";
+export default {
+    components: {
+        headNav: head,
+        leftNav: left
+    },
+    data() {
+        return {};
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+.home {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    //左侧导航定宽
+    @media screen and (min-width: 500px) {
+        .contentL {
+            width: 300px;
+        }
+    }
+
+    //右侧内容定位
+    .contentR {
+        width: 72%;
+    }
+}
+</style>
