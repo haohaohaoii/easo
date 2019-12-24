43505575fb4e7784cd8363336a1ea5d1eb73204b
haohaohaoii <641882555@qq.com>
2019-12-24 10:46:16 +0800

    修改登陆页面为百分百布局

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -3,9 +3,14 @@
     <div class="login">
         <!--登录页面中间内容-->
         <div class="loginContent">
-            <img src="../../assets/images/loginpic.png" alt />
+            <div class="contentL">
+            </div>
             <div class="contentR">
-                <p class="tit">环境管家后台管理系统</p>
+                <div class="tit">
+                     <p>环境管家后台管理系统</p>
+                     <div></div>
+                </div>
+               
                 <div class="loginFill">
                     <el-input placeholder="用户名" v-model="userName" clearable class="iptv"></el-input>
                     <el-input placeholder="密码" v-model="userPaw" show-password class="iptv"></el-input>
@@ -45,48 +50,69 @@ export default {
     overflow: hidden;
 
     background-size: cover;
-
-    .loginContent {
-        width: 816px;
+    /**登陆框 */
+    .loginContent{
         display: flex;
         margin: 0 auto;
-        transform: translateY(-50%);
+        width: 80%;
+        height: 75%;
+        align-items: center;
         position: relative;
         top: 50%;
-        img {
-            width: 408px;
-            height: 450px;
+        transition: transform;
+        transform: translateY(-50%);
+
+        /**登陆框左侧背景 */
+        .contentL{
+            background: url("../../assets/images/loginpic.png") no-repeat;
+            width: 100%;
+            height: 100%;
+            background-size: cover;
         }
-        .contentR {
-            width: 408px;
+        /**登陆框右侧 */
+        .contentR{
+            background: url("../../assets/images/logc.png") no-repeat;
+            width: 100%;
+            height: 100%;
+            background-size: cover;
             display: flex;
             flex-direction: column;
             justify-content: space-around;
             align-items: center;
-            // background-color: #2d55ea;
-            background: url("../../assets/images/logc.png") no-repeat;
-            .tit {
-                font-size: 30px;
-                font-family: mf-lihei;
-                font-weight: 400;
-                color: rgba(255, 255, 255, 1);
-            }
-            .loginFill {
-                padding: 0 35px;
-                .tip {
-                    color: rgba(255, 255, 255, 1);
+            .tit{
+                p{
+                    font-size:54px;
+                    font-family:MF LiHei (Noncommercial);
+                    font-weight:400;
+                    color:rgba(255,255,255,1);
                 }
-                .iptv {
-                    padding-bottom: 10px;
+                div{
+                    width: 10%;
+                    height: 2%;
+                    position: relative;
+                    left: 46%;
+                    top: 11%;
+                    background:rgba(255,255,255,1);
                 }
             }
-
-            .btn {
-                width: 163px;
-                font-size: 17px;
-                font-weight: normal;
-                color: rgba(51, 51, 51, 1);
+            /**登陆框中间部分 */
+            .loginFill{
+                width: 80%;
+                height: 30%;
+                display: flex;
+                flex-direction: column;
+                justify-content: space-around;
+                .tip{
+                    color: white;
+                }
             }
+            /**登陆框按钮 */
+            .btn{
+                width: 60%;
+            }
+        }
+        .contentR >>>.el-input .el-input__clear{
+            color: red;
         }
     }
 }
