75021401dccf5b19d6d6f3586a72c1cffd6f55b5
haohaohaoii <641882555@qq.com>
2020-01-13 15:06:57 +0800

    亲测element-ui切换皮肤

--- src/api/base.js
+++ src/api/base.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /**
  * 接口域名的管理
  */
@@ -8,14 +7,3 @@ const base = {
 }
 
 export default base;
-=======
-/**
- * 接口域名的管理
- */
-const base = {
-    // localUrl: 'http://172.16.23.5:8081'  //本地
-    localUrl: 'http://116.255.130.171:8081'  //公网测试地址
-}
-
-export default base;
->>>>>>> 亲测element-ui切换皮肤
