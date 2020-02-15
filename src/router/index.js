886b8de0ed6b99eba2220263bf4ca0b3643cb89f
haohaohaoii <641882555@qq.com>
2020-02-15 17:33:52 +0800

    家里,不知道什么情况路由router中少了两句动态加载面包屑的方法

--- src/router/index.js
+++ src/router/index.js
@@ -130,6 +130,9 @@ router.beforeEach((to, from, next) => {
         if (to.path == '/login') {
             next('/')
         } else {
+            let defaultVal = to.path.substr(1)
+            store.commit('getBreadlist', to)
+            store.commit('changeDefaultmenu', defaultVal)
             next()
 
         }
